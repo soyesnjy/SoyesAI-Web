015c3686bb3f90a2045b4f5d1ecd6edfc429b142
mars <mars.soyekids@gmail.com>
2025-11-13 11:44:07 +0900

    캐릭터 2개 추가, 이미지 오류 수정

---
+++ pages/maru/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MaruPage = () => {
+  return (
+    <div>
+      <h1>Maru Page</h1>
+      <p>This is a placeholder page for Maru.</p>
+    </div>
+  );
+};
+
+export default MaruPage;
---
+++ pages/nella/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NellaPage = () => {
+  return (
+    <div>
+      <h1>Nella Page</h1>
+      <p>This is a placeholder page for Nella.</p>
+    </div>
+  );
+};
+
+export default NellaPage;
